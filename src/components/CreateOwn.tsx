88377e67d5fa9ab0a25369d1eb28ab4ca256b7e5
SaidARSLAN <lost.myreligion@hotmail.com>
2023-11-18 14:26:59 +0300

    Added navigation buttons on createown

--- src/components/CreateOwn.tsx
+++ src/components/CreateOwn.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const CreateOwn = () => {
+
+
+  const handleClick = (event:React.MouseEvent<HTMLElement>) => {
+
+    const btns = document.getElementsByClassName("btn")
+    for (let i of btns) {
+      if (i === event.target) {
+          i.classList.add("active")
+      }
+      else {
+        i.classList.remove("active")
+
+      }
+    }
+
+  }
+
   return (
-    <div>CreateOwn</div>
+    <div className='create-own'>
+        <div className='title-buttons'>
+            <button className='btn' onClick={handleClick}>Accomodations</button>
+            <button className='btn' onClick={handleClick}>Hotel</button>
+            <button className='btn' onClick={handleClick}>Home/Suit</button>
+        </div>
+    </div>
   )
 }
 
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import CreateOwn from '../components/CreateOwn'
+import { wallpaperOne } from '../contains'
 
 const Hero = () => {
   return (
     <section className='main-hero'>
-      <div className='hero-image'>
+        <div className="image-section">
 
-      </div>
-          <h1>Classical Houses, More than vacation</h1>
-          <CreateOwn />
+        </div>
+        <h1>Search Best Vacation For You</h1>
+        <CreateOwn />
     </section>
   )
 }
