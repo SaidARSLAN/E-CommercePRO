0dd6d754e2f8049bddcf47db5335926f93a43d53
SaidARSLAN <lost.myreligion@hotmail.com>
2023-11-18 22:11:59 +0300

    Refresh application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
-import Search from './components/Search'
 
 function App() {
 
   return (
     <main>
-          <Search />
     </main>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { Provider } from './context/MainContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider>
     <App />
-    </Provider>
 )
