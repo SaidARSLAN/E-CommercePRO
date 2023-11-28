2574d48ce7801f9fb4c3ef6576cffcf26e3f0d52
SaidARSLAN <lost.myreligion@hotmail.com>
2023-11-28 22:05:47 +0300

    Added sidebar logout button

--- components/sidebar/page.tsx
+++ components/sidebar/page.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 
 
 const SideBar = () => {
-    const {loginControl,toggle,getToggleControl} = useContext(GlobalContext)
+    const {loginControl,toggle,getToggleControl,completeLogout} = useContext(GlobalContext)
     const handleClose = () => {
 
         getToggleControl()
@@ -27,7 +27,7 @@ const SideBar = () => {
             <button><CiSearch /></button>
         </div>
         <nav className={style.links}>
-                    {loginControl ? <Link href='/'>Logout</Link> : <Link href='/login'>Login</Link>}
+                    {loginControl ? <Link href='/' onClick={() => completeLogout()}>Logout</Link> : <Link href='/login'>Login</Link>}
                     <Link href='/box'>Box</Link>
         </nav>
         <div>
