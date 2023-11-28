02327034b2600035762d57816677e4f580e1b91b
SaidARSLAN <lost.myreligion@hotmail.com>
2023-11-28 18:35:42 +0300

    Fixed side and navbar background and display properties

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link'
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import styles from './styles.module.css'
 import { CiSearch } from "react-icons/ci";
 import { SiBigcommerce } from "react-icons/si";
@@ -10,11 +10,19 @@ import GlobalContext from '@/context/MainContext';
 const Header = () => {
     
     const {toggle,getToggleControl} = useContext(GlobalContext)
-    
 
+    const [scrollToggle,setScrollToggle] = useState(false)
 
+    window.addEventListener('scroll',event => {
+        if (window.scrollY > 650) {
+            setScrollToggle(true)
+        }
+        else {
+            setScrollToggle(false)
+        }
+    })
     return (
-        <header className={toggle ? styles.headerSide : styles.header}>
+        <header className={toggle ? styles.headerSide : scrollToggle ? styles.headerWhite : styles.header}>
             <div className={styles.logo}>
                 <SiBigcommerce />
             </div>
