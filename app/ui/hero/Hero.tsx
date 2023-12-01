a55d19cb76fb1f132a9c4187d9d85437adf25b2c
SaidARSLAN <lost.myreligion@hotmail.com>
2023-12-02 01:50:27 +0300

    Added hero page image

--- app/ui/hero/Hero.tsx
+++ app/ui/hero/Hero.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './hero.css'
+import Image from 'next/image'
+import kitchen from '../../../assets/kitchen.jpg'
 const Hero = () => {
   return (
     <section className='hero-section'>
-        HERO
+        <Image src={kitchen}  alt='kitchen' width={1920} height={1080}/>
     </section>
   )
 }
