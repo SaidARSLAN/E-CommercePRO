88dd256ded21e3be65b572cd02ae611a7fdb30c7
SaidARSLAN <lost.myreligion@hotmail.com>
2023-12-02 01:54:12 +0300

    Added header link href

--- app/ui/header/Header.tsx
+++ app/ui/header/Header.tsx
@@ -3,6 +3,7 @@ import React, { useContext } from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
 import './header.css'
 import GlobalContext from '@/app/context/MainContext';
+import Link from 'next/link';
 
 const Header = () => {
   const {openSideBarFunction} = useContext(GlobalContext)
@@ -13,7 +14,7 @@ const Header = () => {
   return (
     <nav>
       <div className='nav-logo'>
-        <h1>LOGO</h1>
+        <Link href='/'><h1>LOGO</h1></Link>
       </div>
       <div className='nav-hamburger' onClick={openSideBar}>
       <GiHamburgerMenu />
