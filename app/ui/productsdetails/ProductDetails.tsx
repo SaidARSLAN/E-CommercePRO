a8432e32a060bb38b74d0ff2654b751fc6553e67
SaidARSLAN <lost.myreligion@hotmail.com>
2023-12-03 20:40:58 +0300

    Added read more

--- app/ui/productsdetails/ProductDetails.tsx
+++ app/ui/productsdetails/ProductDetails.tsx
@@ -1,7 +1,14 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import './detail.css'
 const ProductDetails = ({product}:any) => {
+
+  const [readControl, setReadControl] = useState(false)
+  const handleReadMore = () => {
+      setReadControl(!readControl) 
+  }
+
   return (
     <div className='product'>
         <div className='product-title'>
@@ -9,7 +16,8 @@ const ProductDetails = ({product}:any) => {
         <h1>{product.title}</h1>
         </div>
         <div className='product-description'>
-            <p className='descript'>{product.description}</p>
+            <p className={readControl ? 'read':'descript'}>{product.description}</p>
+            <button className='read-more' onClick={handleReadMore}>read more</button>
             <h4>{product.category}</h4>
             <div className='button-section'>
                 <button className='add'>Add</button>
